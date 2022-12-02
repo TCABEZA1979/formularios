aa7940d8710d36b7be51ff12f5250f5bc42de660
Tito Cabeza <TCABEZA@cor.multimax>
2022-12-02 14:21:40 -0400

    vistas

---
+++ resources/views/css.blade.php
@@ -0,0 +1,20 @@
+<link  href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<link href="css/plugins/fontawesome-free/css/all.min.css" rel="stylesheet" />
+<link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<link href="css/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+<link href="css/plugins/icheck-bootstrap/icheck-bootstrap.min.css" rel="stylesheet" />
+<link href="css/plugins/jqvmap/jqvmap.min.css" rel="stylesheet" />
+<link href="css/dist/css/adminlte.min.css" rel="stylesheet" />
+<link href="css/plugins/overlayScrollbars/css/OverlayScrollbars.min.css" rel="stylesheet" />
+<link href="css/plugins/daterangepicker/daterangepicker.css" rel="stylesheet" />
+<link href="css/plugins/summernote/summernote-bs4.min.css"  rel="stylesheet">
+<link href="css/plugins/daterangepicker/daterangepicker.css"  rel="stylesheet">
+<link href="css/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css" rel="stylesheet">
+<link href="css/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet">
+
+<link href="css/plugins/select2/css/select2.min.css" rel="stylesheet">
+<link href="css/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css" rel="stylesheet">
+<link href="css/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css" rel="stylesheet">
+<link href="css/plugins/bs-stepper/css/bs-stepper.min.css" rel="stylesheet">
+<link href="css/plugins/dropzone/min/dropzone.min.css" rel="stylesheet">
+<link href="css/dist/css/adminlte.min.css" rel="stylesheet">
