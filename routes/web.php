16ac86b5847b24a989c628bbcd1dab16d2099d89
TCABEZA1979 <titocafe@gmail.com>
2022-12-10 18:29:59 -0400

    creacion controlador articulo con migracion y modelo

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,13 +17,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/vista', [App\Http\Controllers\dashboard::class, 'index'])->name('vista');
 //aqui se llama al formulario para agregar clientes y modificarlos
 Route::get('/formulario', [App\Http\Controllers\personaController::class, 'index'])->name('formulario');
-//aqui se ejecuta el regitro de personas o clientes
-// Route::POST('/registrarpersona', [App\Http\Controllers\personaController::class, 'store'])->name('/registrarpersona');
 //aqui se ejecuta el contralodr update entre otros
 Route::resource('/personas', App\Http\Controllers\personaController::class);
-
-
-
-
-
-// Route::post('/registrousuario', 'personaController@store');//ejecuta bien
